f1ac90ba8558134e94c7a3d100ca9529c282bc52
gianiaz <gianiaz@gmail.com>
2017-11-04 07:25:23 +0100

    Aggiunto about

--- src/AppBundle/Resources/public/admin/js/admin.js
+++ src/AppBundle/Resources/public/admin/js/admin.js
@@ -2,6 +2,20 @@ $(function() {
 
     var $body = $('body');
 
+    $(document).on('click', '#about', function(e) {
+        e.preventDefault();
+
+        var opts = {
+            'type': 'danger',
+            'titolo': 'Hire me!',
+            'content': 'Looking for an experienced Fullstack developer?<br />Visit my linkedin profile at <a href="https://www.linkedin.com/in/gianiaz/">https://www.linkedin.com/in/gianiaz/</a>',
+            'OK': 'Ok',
+            'callback': null
+        };
+        HandleBarHelper.alert(opts);
+
+    });
+
     /** Click sul bottone di cancellazione nelle liste di elementi **/
     $(document).on('click', '.btn-delete', function(e) {
 
@@ -11,7 +25,7 @@ $(function() {
         var href = $(this).attr('href');
 
         var question = _('Vuoi davvero eliminare l\'elemento "%s"?', 'default', title);
-        
+
         var opts = {
             'type': 'danger',
             'titolo': _('Conferma cancellazione'),
